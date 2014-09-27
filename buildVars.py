e2bce93389ff2c91e2c6db3ab3e71f1d6f534b92
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-09-27 21:07:30 +0200

    Updated template according to ticket 2694.

--- buildVars.py
+++ buildVars.py
@@ -11,22 +11,24 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
-	"addon-name" : "teamViewer",
+	"addon_name" : "teamViewer",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon-summary" : _("TeamViewer"),
+	"addon_summary" : _("TeamViewer"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon-description" : _("""Improves the accessibility of TeamViewer with NVDA.
+	"addon_description" : _("""Improves the accessibility of TeamViewer with NVDA.
 TeamViewer can be used to hold online meetings as well as provide remote support/access.
 For more information about the TeamViewer application, see:
 http://www.teamviewer.com"""),
 	# version
-	"addon-version" : "1.1",
+	"addon_version" : "2.0-dev",
 	# Author(s)
-	"addon-author" : u"Noelia Ruiz Martínez <nrm1977@gmail.com>, Mesar Hameed <mhameed@src.gnome.org>",
+	"addon_author" : u"Noelia Ruiz Martínez <nrm1977@gmail.com>, Mesar Hameed <mhameed@src.gnome.org>",
 	# URL for the add-on documentation support
-	"addon-url" : "http://addons.nvda-project.org"
+	"addon_url" : "http://addons.nvda-project.org",
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
 }
 
 
@@ -34,10 +36,10 @@ import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon", "appModules", "*.py"), os.path.join("addon", "*.py")]
+pythonSources = [os.path.join("addon", "*.py"), os.path.join("addon", "appModules", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+i18nSources = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
